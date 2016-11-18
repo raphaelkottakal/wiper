ad10cad146a619a8f2d6f8799881738483d021bb
Raphael Kottakal <raphaelsemail@gmail.com>
2016-11-18 15:57:20 +0530

    get the mist back

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
 		this.ctx = this.canvas.getContext('2d');
 		this.w = this.canvas.width = window.innerWidth;
 		this.h = this.canvas.height = this.w * 0.75;
+		this.animationDone = true;
 		// this.bothImagesLoaded = false;
 
 		// this.setState({
@@ -34,7 +35,7 @@ class App extends Component {
 		this.ctx.fillText('Loading',this.w/2, this.h/2 + 10);
 
 		let image = new Image();
-		let mist = new Image();
+		this.mist = new Image();
 		let imageLoaded = false;
 		let mistLoaded = false;
 
@@ -45,23 +46,23 @@ class App extends Component {
 				this.setState({bothImagesLoaded: true});
 				// this.bothImagesLoaded = true;
 				// this.ctx.drawImage(image,0,0,this.w,this.h);
-				this.ctx.drawImage(mist,0,0,this.w,this.h);
+				this.ctx.drawImage(this.mist,0,0,this.w,this.h);
 			}
 		};
 
-		mist.onload = ()=>{
+		this.mist.onload = ()=>{
 			mistLoaded = true;
 			if (imageLoaded && mistLoaded) {
 				// animate();
 				this.setState({bothImagesLoaded: true});
 				// this.bothImagesLoaded = true;
 				// this.ctx.drawImage(image,0,0,this.w,this.h);
-				this.ctx.drawImage(mist,0,0,this.w,this.h);
+				this.ctx.drawImage(this.mist,0,0,this.w,this.h);
 			}
 		};
 
 		image.src = 'http://assets.myntassets.com/v1479297908/radium/wiper/ImageClean.jpg';
-		mist.src = 'http://assets.myntassets.com/v1479297909/radium/wiper/ImageMyst.jpg';
+		this.mist.src = 'http://assets.myntassets.com/v1479297909/radium/wiper/ImageMyst.jpg';
 		
 		const animate = () => {
 			window.requestAnimationFrame( animate );
@@ -89,7 +90,8 @@ class App extends Component {
 	// }
 
 	handelTouch(e) {
-		if (this.state.bothImagesLoaded) {
+		clearTimeout(this.mistTimeout);
+		if (this.state.bothImagesLoaded && this.animationDone) {
 
 			const r = 20;
 
@@ -99,11 +101,39 @@ class App extends Component {
 				this.ctx.globalCompositeOperation = 'destination-out';
 				this.ctx.beginPath();
 				this.ctx.arc(clientX,clientY, r, 0, 2*Math.PI);
-				this.ctx.fill();				
+				this.ctx.fill();
 				this.ctx.globalCompositeOperation = 'source-over';
 			}
 
 		}
+	}
+
+	reRenderMist() {
+
+		let alpha = 0;
+
+		const animate = () => {
+			this.animationDone = false;
+			alpha += 0.02;
+			this.ctx.globalAlpha = alpha;
+			this.ctx.drawImage(this.mist,0,0,this.w,this.h);
+			if (alpha >= 1) {
+				console.log('stop animation');
+				this.animationDone = true;
+				cancelAnimationFrame(this.animationRequest);
+			} else {
+
+				requestAnimationFrame(animate);
+			}
+		}
+
+		this.animationRequest = requestAnimationFrame(animate);
+		console.log('rerender mist');
+
+	}
+
+	handelTouchEnd(e) {
+		this.mistTimeout = setTimeout(this.reRenderMist.bind(this), 1000);
 	}
 
 
@@ -131,7 +161,7 @@ class App extends Component {
         Wipe the mist
         <div style={css.wrapper}>
         	<img alt="background" style={css.image} src="http://assets.myntassets.com/v1479297908/radium/wiper/ImageClean.jpg" />
-        	<canvas style={css.canvas} onTouchMove={this.handelTouch.bind(this)} ref="myCanvas"></canvas>
+        	<canvas style={css.canvas} onTouchEnd={this.handelTouchEnd.bind(this)} onTouchMove={this.handelTouch.bind(this)} ref="myCanvas"></canvas>
         </div>
         <div>Height: {this.state.cHeight}</div>
         <div>Width: {this.state.cWidth}</div>
